b28e57bed494059a3427156a29f8952306e8145d
scprogramming <scottc130@gmail.com>
2021-01-14 19:46:54 -0500

    Merging config and login

--- login.php
+++ login.php
@@ -1,7 +1,19 @@
 <?php
 
-   include("config.php");
+	/* Database credentials. Assuming you are running MySQL
+	server with default setting (user 'root' with no password) */
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'test');
 
+	/* Attempt to connect to MySQL database */
+	$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+	// Check connection
+	if($db === false){
+	die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
    
 
    if($_SERVER["REQUEST_METHOD"] == "POST") {
